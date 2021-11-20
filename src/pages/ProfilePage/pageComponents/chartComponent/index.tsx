64851d4eea3b6e2885acd1ef330d37342f87a50d
DaniloGempe <danilo@gempe.dev>
2021-11-20 15:21:02 -0300

    Fix Readonly class component type declaration error

--- src/pages/ProfilePage/pageComponents/chartComponent/index.tsx
+++ src/pages/ProfilePage/pageComponents/chartComponent/index.tsx
@@ -1,59 +1,74 @@
-import React from 'react';
-import ReactApexChart from 'react-apexcharts'
+import React, { useState } from "react";
+import ReactApexChart from "react-apexcharts";
 
-import './styles.css';
+import "./styles.css";
 
-class ApexChart extends React.Component {
-    constructor(props:any) {
-      super(props);
-
-      this.state = {
-      
-        series: [{
-            name: "Desktops",
-            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-        }],
-        options: {
-          chart: {
-            height: 350,
-            type: 'line',
-            zoom: {
-              enabled: false
-            }
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            curve: 'straight'
-          },
-          title: {
-            text: '',
-            align: 'left'
-          },
-          grid: {
-            row: {
-              colors: ['#f3f3f3', 'transparent'],
-              opacity: 0.5
-            },
-          },
-          xaxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-          }
+const ApexChart: React.FC = () => {
+  const [state, setState] = useState({
+    series: [
+      {
+        name: "Desktops",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false,
         },
-      };
-    }
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "straight",
+      },
+      title: {
+        text: "",
+        align: "left",
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"],
+          opacity: 0.5,
+        },
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+        ],
+      },
+    },
+  });
 
-    render() {
-      return (
-        <>
-          <div id="chart">
-            <ReactApexChart options={this.state.options as any} series={this.state.series} type="line" height={350} />
-          </div>
-        </>
-      )
-        
-    }
-}
+  //constructor(props: any) {
+  //   super(props);
+  //
+  //   this.state = ;
+  // }
+
+  return (
+    <>
+      <div id="chart">
+        <ReactApexChart
+          options={state.options as any}
+          series={state.series}
+          type="line"
+          height={350}
+        />
+      </div>
+    </>
+  );
+};
 
 export default ApexChart;
