0817906893756a26981a2ca5c43fd26f4b762a79
luanda-vital <luavital0@gmail.com>
2021-11-20 19:29:29 -0300

    ajuste de imports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,17 @@
-import LandingPage from './landingPage'
-import PageAulas from "./pageAulas"
-import ProfilePage  from './ProfilePage'
-import PageQuiz from './pageQuiz'
-import ResultadoQuiz from './pageResultado'
-import TrilhaTutorial from './trilhaTutorial'
+import LandingPage from './landingPage';
+import PageAulas from './pageAulas';
+import ProfilePage  from './ProfilePage';
+import PageQuiz from './pageQuiz';
+import ResultadoQuiz from './pageResultado';
+import TrilhaTutorial from './trilhaTutorial';
+import Dashboard from './Dashboard';
 
-export{ PageAulas, LandingPage, PageQuiz, ResultadoQuiz, ProfilePage, TrilhaTutorial }
+export { 
+  PageAulas, 
+  LandingPage, 
+  PageQuiz, 
+  ResultadoQuiz, 
+  ProfilePage, 
+  TrilhaTutorial,
+  Dashboard
+}
